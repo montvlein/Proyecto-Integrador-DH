074ebd29f935098868d1bf60593ebd4b63a11b49
Fabricio Montivero <fabricio9027@gmail.com>
2022-10-20 15:58:05 -0400

    [AGREGADO] #4 - se agrego propiedades, @getter y @setter de la clase

--- DigitalBooking/src/main/java/com/dh/DigitalBooking/Models/Entities/Categoria.java
+++ DigitalBooking/src/main/java/com/dh/DigitalBooking/Models/Entities/Categoria.java
@@ -1,4 +1,18 @@
 package com.dh.DigitalBooking.Models.Entities;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class Categoria {
+
+    private String id ;
+    private String Titulo;
+    private String Descripcion ;
+    private String urlImagen;
+
+    public Categoria(){}
+
+    public Categoria(String id, String Titulo, String Descipcion, String urlImagen){}
 }
