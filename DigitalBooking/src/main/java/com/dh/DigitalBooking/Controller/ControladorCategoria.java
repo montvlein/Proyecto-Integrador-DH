99f195b43a849969516e17a774d8d86f11a8e923
Fabricio Montivero <fabricio9027@gmail.com>
2022-10-20 16:36:29 -0400

    [MODIFICADO] #4 - se agrega propiedad servicio y setter con bean

--- DigitalBooking/src/main/java/com/dh/DigitalBooking/Controller/ControladorCategoria.java
+++ DigitalBooking/src/main/java/com/dh/DigitalBooking/Controller/ControladorCategoria.java
@@ -1,4 +1,17 @@
 package com.dh.DigitalBooking.Controller;
 
+import com.dh.DigitalBooking.Services.ServicioCategoria;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class ControladorCategoria {
+
+    private ServicioCategoria servicio;
+
+    @Autowired
+    public void setServicio(ServicioCategoria servicio) {
+        this.servicio = servicio;
+    }
+
 }
