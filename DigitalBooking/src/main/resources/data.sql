c2f27f902e1a93b413cde720b37d94686c5aec27
Fabricio Montivero <fabricio9027@gmail.com>
2022-10-21 18:33:46 -0400

    [MODIFICADO] #3 - se elimino texto extra

--- DigitalBooking/src/main/resources/data.sql
+++ DigitalBooking/src/main/resources/data.sql
@@ -3,4 +3,4 @@ INSERT INTO categorias(titulo, descripcion, url_img) VALUES('Sedan' , 'Chevrolet
 INSERT INTO categorias(titulo, descripcion, url_img) VALUES('SUV', 'Volkswagen Taos o similar', 'https://i.ibb.co/Sr3msZY/SUV.jpg');
 INSERT INTO categorias(titulo, descripcion, url_img) VALUES('Lujo','Mercedes-Benz Serie E o similar','https://i.ibb.co/BcgYwnh/Lujo.png');
 INSERT INTO categorias(titulo, descripcion, url_img) VALUES('Pickup','Toyota Hilux o similar','https://i.ibb.co/0rMj8S9/Pickup.png');
-INSERT INTO categorias(titulo, descripcion, url_img) VALUES('Van','Toyota Hiace o similar','https://i.ibb.co/QJhsV3V/Van.png');o/wzczdV5/Compacto.png');
+INSERT INTO categorias(titulo, descripcion, url_img) VALUES('Van','Toyota Hiace o similar','https://i.ibb.co/QJhsV3V/Van.png');
