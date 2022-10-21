d62f88d44572cf7fe26960d8dc6eeebc06c9f5c1
Fabricio Montivero <fabricio9027@gmail.com>
2022-10-21 18:35:15 -0400

    [MODIFICADO] #3 - se agregaron beans para nombrar las columnas y tabla de la base de datos

--- DigitalBooking/src/main/java/com/dh/DigitalBooking/Models/Entities/Categoria.java
+++ DigitalBooking/src/main/java/com/dh/DigitalBooking/Models/Entities/Categoria.java
@@ -3,21 +3,21 @@ package com.dh.DigitalBooking.Models.Entities;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Getter
 @Setter
 @Entity
+@Table(name = "CATEGORIAS")
 public class Categoria {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id ;
     private String Titulo;
     private String Descripcion ;
+
+    @Column(name = "url_img")
     private String urlImagen;
 
 }
