1d98f54d3f4d9e634e1c5877a142f2b19a39e271
Fabricio Montivero <fabricio9027@gmail.com>
2022-10-23 19:35:52 -0400

    [MODIFICADO] se agrego libreria react-router-dom y componente BrowserRouter

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 import { AppContext } from "./contexto/AppContext"
+import { BrowserRouter } from 'react-router-dom'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <AppContext>
-      <App/>
+      <BrowserRouter>
+        <App/>
+      </BrowserRouter>
     </AppContext>
   </React.StrictMode>
 )
