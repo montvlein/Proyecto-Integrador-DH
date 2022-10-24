a459148e197435e82c18a0ec3bc0f3290bc75a52
Julian Carbonetti <julian.carbonetti@gmail.com>
2022-10-24 20:19:21 -0300

    [NUEVO] #5 Se agregaron endpoints para categorias

--- DigitalBooking/src/main/java/com/dh/DigitalBooking/Controller/ControladorCategoria.java
+++ DigitalBooking/src/main/java/com/dh/DigitalBooking/Controller/ControladorCategoria.java
@@ -1,10 +1,17 @@
 package com.dh.DigitalBooking.Controller;
 
+import com.dh.DigitalBooking.Models.Entities.Categoria;
 import com.dh.DigitalBooking.Services.ServicioCategoria;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("v1/categoria")
 public class ControladorCategoria {
 
     private ServicioCategoria servicio;
@@ -13,5 +20,16 @@ public class ControladorCategoria {
     public void setServicio(ServicioCategoria servicio) {
         this.servicio = servicio;
     }
+
+    @GetMapping("listarTodos")
+    public ResponseEntity<?> listarTodos() throws Exception{
+        return ResponseEntity.ok(servicio.listar());
+    };
+
+    @PostMapping("nuevaCategoria")
+    public ResponseEntity<?> nuevaCategoria(@RequestBody Categoria c) throws Exception{
+        return ResponseEntity.ok(servicio.guardar(c));
+    }
+
 
 }
