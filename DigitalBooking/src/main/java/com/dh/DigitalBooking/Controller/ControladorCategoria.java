fe86c408a43e8bc987547d4539e837bcb972eadc
Julian Carbonetti <julian.carbonetti@gmail.com>
2022-10-25 20:06:30 -0300

    [ACTUALIZADO] #5 Se terminaron Endpoints

--- DigitalBooking/src/main/java/com/dh/DigitalBooking/Controller/ControladorCategoria.java
+++ DigitalBooking/src/main/java/com/dh/DigitalBooking/Controller/ControladorCategoria.java
@@ -3,12 +3,10 @@ package com.dh.DigitalBooking.Controller;
 import com.dh.DigitalBooking.Models.Entities.Categoria;
 import com.dh.DigitalBooking.Services.ServicioCategoria;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("v1/categoria")
@@ -24,12 +22,28 @@ public class ControladorCategoria {
     @GetMapping("listarTodos")
     public ResponseEntity<?> listarTodos() throws Exception{
         return ResponseEntity.ok(servicio.listar());
-    };
+    }
 
     @PostMapping("nuevaCategoria")
     public ResponseEntity<?> nuevaCategoria(@RequestBody Categoria c) throws Exception{
-        return ResponseEntity.ok(servicio.guardar(c));
+       return ResponseEntity.ok(servicio.guardar(c));
     }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> eliminarPorId(@PathVariable Long id) throws Exception {
+        servicio.eliminarPorId(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping
+    public ResponseEntity<?> editar(@RequestBody Categoria categoria) throws Exception {
+            ResponseEntity<?> response = ResponseEntity.notFound().build();
+            if (categoria.getId() != null && servicio.buscarPorId(categoria.getId())!= null){
+        response = ResponseEntity.ok(servicio.actualizar(categoria));
+            }
+        return response;
+    }
+
+
 
 
 }
--- DigitalBooking/src/main/java/com/dh/DigitalBooking/Models/Entities/Categoria.java
+++ DigitalBooking/src/main/java/com/dh/DigitalBooking/Models/Entities/Categoria.java
@@ -20,4 +20,5 @@ public class Categoria {
     @Column(name = "url_img")
     private String urlImagen;
 
+
 }
