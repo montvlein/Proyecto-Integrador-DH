d21e1db085d3348d2bfda9149552cc2a00e1f95e
Fabricio Montivero <fabricio9027@gmail.com>
2022-10-27 00:41:48 -0400

    [MODIFICADO] se agrego validacion para el inicio de sesion

--- client/src/components/usuarioLogin/login.jsx
+++ client/src/components/usuarioLogin/login.jsx
@@ -1,17 +1,45 @@
 import styles from "../../styles/formularios.module.css";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import Contexto from "../../contexto/AppContext"
+import { useNavigate } from "react-router-dom"
 
 const Login = () => {
-  const { iniciarSesion } = useContext(Contexto)
+  const redirigir = useNavigate()
+  const { iniciarSesion, validarUsuario } = useContext(Contexto)
+  const [invalido, setInvalido] = useState(false)
+
   const handleSubmit = function (e) {
     e.preventDefault()
-    iniciarSesion(e.target.elements.email.value, e.target.elements.password.value)
+    let [mail, pass] = [e.target.elements.email.value, e.target.elements.password.value]
+    if (validarUsuario(mail, pass)) {
+      iniciarSesion(mail)
+      redirigir("/")
+    } else {
+      setInvalido(true)
+    }
+  }
+
+  if (invalido) {
+    return(
+      <div className={styles.divContainer}>
+        <Formulario ingresar={handleSubmit}/>
+        <Errores/>
+      </div>
+    )
   }
 
     return(
-        <div className={styles.divContainer}>
-        <form className={styles.formularioContainer} onSubmit={handleSubmit}>
+      <div className={styles.divContainer}>
+        <Formulario ingresar={handleSubmit}/>
+      </div>
+    )
+  }
+
+export default Login;
+
+function Formulario({ingresar}) {
+  return(
+    <form className={styles.formularioContainer} onSubmit={ingresar} method="POST">
           <div className={styles.contenidoFormulario}>
             <h3 className={styles.tituloFormulario}>Iniciar Sesión</h3>
             <div className="form-group mt-3">
@@ -41,26 +69,14 @@ const Login = () => {
                 ¿Aún no tenes cuenta? <a href="/crearCuenta">Registrate</a> {/*agregar ruta de registro*/}
             </p>
           </div>
-        </form>
-      </div>
-    )
-  }
+    </form>
+  )
+}
 
-
-export default Login;
-
-
-
-
-/*
-Validaciones en inputs: El formulario debe validar que:
-
-El campo de email sea un email válido
-Que la contraseña tenga más de 6 caracteres.
-Que las contraseñas coincidan
-
-Botón Crear Cuenta: Al dar click que funcione la validación. Para esto almacenar un correo electrónico y contraseña de prueba en un objeto y comparar con estos datos los datos ingresados.
-Credenciales inválidas: Si falla la validación el formulario debe indicar “Por favor vuelva a intentarlo, sus credenciales son inválidas”.
-Credenciales válidas: Se simulará que el usuario está logueado. Desaparecerá el formulario de login volveremos a la Home inicial pero en el header a la derecha, en vez de ver los botones de inicio de sesión y registro veremos: Hola [nombre_de_usuario], un link de cerrar sesión y un avatar circular con las iniciales del usuario.
-
-*/
+function Errores() {
+  return(
+    <ul className={styles.error}>
+        <li >Por favor vuelva a intentarlo, sus credenciales son inválidas</li>
+    </ul>
+  )
+}
--- client/src/contexto/AppContext.jsx
+++ client/src/contexto/AppContext.jsx
@@ -1,53 +1,78 @@
 import React, { useState, createContext } from "react";
-import categorias from "../data/categorias.json"
-import { Usuario } from "../modelos/usuario.js"
-const Contexto = createContext()
+import categorias from "../data/categorias.json";
+import { Usuario } from "../modelos/usuario.js";
+const Contexto = createContext();
 
-export function AppContext({children}) {
-    let opciones = (informacion, metodo='POST', tipo='application/json') => {
-        return {
-            method: metodo,
-            headers: {
-                'Content-Type': tipo
-            },
-            body: JSON.stringify(informacion)
-        }
-    }
+export function AppContext({ children }) {
+  let opciones = (informacion, metodo = "POST", tipo = "application/json") => {
+    return {
+      method: metodo,
+      headers: {
+        "Content-Type": tipo,
+      },
+      body: JSON.stringify(informacion),
+    };
+  };
 
-    // usuario y sesion
-    const [ listaUsuarios, setListaUsuarios ] = useState([])
-    const [ sesionIniciada, setSesionIniciada ] = useState(false)
-    const [ usuario, setUsuario ] = useState()
-    function getUsuario() { return usuario }
-    function getListaUsuarios() { return listaUsuarios }
-    function estaLaSesionIniciada() {return sesionIniciada}
+  // usuario y sesion
+  const [listaUsuarios, setListaUsuarios] = useState([]);
+  const [sesionIniciada, setSesionIniciada] = useState(false);
+  const [usuario, setUsuario] = useState();
+  function getUsuario() {
+    return usuario;
+  }
+  function getUsuarioPorMail(mail) {
+    return getListaUsuarios().find((usuario) => usuario.mail === mail);
+  }
+  function validarUsuario(mail, pass) {
+    let usuarioIngresando = getUsuarioPorMail(mail);
+    return usuarioIngresando?.pass === pass;
+  }
+  function getListaUsuarios() {
+    return listaUsuarios;
+  }
+  function estaLaSesionIniciada() {
+    return sesionIniciada;
+  }
 
-    function registrarUsuario(usuario) {
-        getListaUsuarios().push(usuario)
-        console.table(getListaUsuarios())
-        setUsuario(usuario)
-        setSesionIniciada(true)
-    }
+  function registrarUsuario(usuario) {
+    getListaUsuarios().push(usuario);
+    setUsuario(usuario);
+    setSesionIniciada(true);
+  }
 
-    function iniciarSesion(mail, pass) {
-        let usuario = new Usuario("Maigualida","Delgado", mail)
-        setUsuario(usuario)
-        setSesionIniciada(true)
-    }
-    function cerrarSesion() {
-        setUsuario({})
-        setSesionIniciada(false)
-    }
+  function iniciarSesion(mail) {
+    let usuario = getUsuarioPorMail(mail);
+    setUsuario(usuario);
+    setSesionIniciada(true);
+  }
 
-    // categorias
-    const [ listaCategorias, setCategorias ] = useState(categorias)
-    function getListaCategorias() { return listaCategorias }
+  function cerrarSesion() {
+    setUsuario({});
+    setSesionIniciada(false);
+  }
 
-    return(
-        <Contexto.Provider value={{ getUsuario, estaLaSesionIniciada, iniciarSesion, cerrarSesion, getListaCategorias,registrarUsuario }}>
-            {children}
-        </Contexto.Provider>
-    )
+  // categorias
+  const [listaCategorias, setCategorias] = useState(categorias);
+  function getListaCategorias() {
+    return listaCategorias;
+  }
+
+  return (
+    <Contexto.Provider
+      value={{
+        getUsuario,
+        estaLaSesionIniciada,
+        iniciarSesion,
+        cerrarSesion,
+        registrarUsuario,
+        validarUsuario,
+        getListaCategorias,
+      }}
+    >
+      {children}
+    </Contexto.Provider>
+  );
 }
 
-export default Contexto
+export default Contexto;
