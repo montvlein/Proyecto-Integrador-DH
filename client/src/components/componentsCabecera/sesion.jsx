d5ffd9640b09df0aa92420d1667a8bf9ac781657
Fabricio Montivero <fabricio9027@gmail.com>
2022-10-27 01:12:24 -0400

    [MODIFICADO] reordenando y renombrando archivos

--- client/src/components/componentsCabecera/sesion.jsx
+++ client/src/components/componentsCabecera/sesion.jsx
@@ -1,10 +1,12 @@
 import { useContext } from "react";
 import Contexto from "../../contexto/AppContext"
-import { Link } from 'react-router-dom'
-import UsuarioIniciado from "./sesionIniciada";
+import { Link, useLocation } from 'react-router-dom'
+import UsuarioIniciado from "./sesionUsuario";
 
 function Sesion() {
     const { estaLaSesionIniciada } = useContext(Contexto);
+    const location = useLocation()
+
 
     if(estaLaSesionIniciada()) {
         return (
@@ -13,8 +15,8 @@ function Sesion() {
     } else {
         return (
             <div class="text-end">
-                <Link to="/iniciarSesion" class="btn btn-outline-light me-2">Inicia sesión</Link>
-                <Link to="/crearcuenta" class="btn btn-warning">Registrate</Link>
+                <Link to="/iniciarSesion" className={ location.pathname === "/iniciarSesion" ? "d-none btn btn-outline-light me-2":"btn btn-outline-light me-2"}>Inicia sesión</Link>
+                <Link to="/crearcuenta" className={ location.pathname === "/crearcuenta" ? "d-none btn btn-warning":"btn btn-warning"}>Registrate</Link>
             </div>
         )
     }
