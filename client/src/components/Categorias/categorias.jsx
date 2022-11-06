ad448015c893494f858c3312f8827186c0a31716
Fabricio Montivero <fabricio9027@gmail.com>
2022-11-06 00:07:49 -0400

    [MODIFICADO] se realizo conexion con el api. Se agrego esqueleto de carga

--- client/src/components/Categorias/categorias.jsx
+++ client/src/components/Categorias/categorias.jsx
@@ -1,30 +1,65 @@
-import { useContext } from "react"
-import Contexto from "../../contexto/AppContext"
+import { useEffect, useState, useContext } from "react"
 import styles from "./categorias.module.css"
+import Contexto from "../../contexto/AppContext";
+import { DigitalBookingApi } from "../../data/conexionAPI";
 
 export default function Categorias() {
-    const { getListaCategorias, filtarAutos } = useContext(Contexto)
+    const [cargando, setEstaCargando] = useState(true)
+    const [categorias, setCategorias] = useState([])
+    const { filtarAutos }  = useContext(Contexto)
+
+    useEffect(()=>{
+        DigitalBookingApi
+        .categoria
+        .listarTodos()
+        .then(categorias => {
+            setCategorias(categorias)
+            setEstaCargando(false)
+        })
+    },[])
+
+    if (cargando) {
+        return(
+            <section className={styles.contenedorPadre}>
+                <div className={styles.tituloCategorias}>
+                <div><h3>Elige tu categoria</h3></div> <div><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit</p></div>
+                </div>
+
+                <div className={styles.categoria_section}>
+                    {/* opcion 1 - spinner */}
+                    <article className={`${styles.categoria_article}`}>
+                        <div className={`spinner-border text-warning ${styles.categoria_loading_font}`} role="status"></div>
+                    </article>
+                    {/* opcion 2 - brillo */}
+                    <article className={`loading_background ${styles.categoria_loading_height}`}>
+                        <div className={`loading_animation`}></div>
+                    </article>
+                </div>
+            </section>
+        )
+    }
 
     return(
-    <section className={styles.contenedorPadre}>
-    <div className={styles.tituloCategorias}>
-    <div><h3>Elige tu categoria</h3></div> <div><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit</p></div>
-    </div>
+        <section className={styles.contenedorPadre}>
+            <div className={styles.tituloCategorias}>
+                <div><h3>Elige tu categoria</h3></div>
+                <div><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit</p></div>
+            </div>
 
-    <div className={styles.categoria_section}>
-        {
-            getListaCategorias().map( cat => {
-             return(
-                <article className={styles.categoria_article} key={cat.ID} onClick={(e) => { filtarAutos(cat.TITULO)}}>
-                    <figure className={styles.categoria_figure}>
-                        <img src={cat.URL_IMG} alt={cat.DESCRIPCION} className={styles.categoria_img}/>
-                        <figcaption className={styles.categoria_figcaption}> {cat.TITULO} </figcaption>
-                    </figure>
-                </article>
-                )
-            })
-        }
-        </div>
-    </section>
+            <div className={styles.categoria_section}>
+            {
+                categorias.map( cat => {
+                 return(
+                    <article className={styles.categoria_article} key={cat.id} onClick={(e) => { filtarAutos(cat.titulo)}}>
+                        <figure className={styles.categoria_figure}>
+                            <img src={cat.urlImagen} alt={cat.titulo} className={styles.categoria_img}/>
+                            <figcaption className={styles.categoria_figcaption}> {cat.titulo} </figcaption>
+                        </figure>
+                    </article>
+                    )
+                })
+            }
+            </div>
+        </section>
     )
 }
