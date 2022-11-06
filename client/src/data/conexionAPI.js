4b800d243d19152a2f8a713b22c070b19fefeb6c
Fabricio Montivero <fabricio9027@gmail.com>
2022-11-05 21:23:33 -0400

    [MODIFICADO] la clase ahora tiene una propiedad para el endpoint de categoria

--- client/src/data/conexionAPI.js
+++ client/src/data/conexionAPI.js
@@ -1,21 +1,21 @@
-class API {
+class DigitalBookingAPI {
 
     constructor(basepath="http://localhost:8080/") {
         this.basepath = basepath
+        this.categoria = new CategoriaEndPoint(this.basepath, "v1/categoria")
+    }
+
+}
+
+class CategoriaEndPoint {
+    constructor(basepath, categoriaUri="api/v1/categoria") {
+        this.categoriaUri = categoriaUri
+        this.uri = `${basepath}${this.categoriaUri}`
     }
 
     listarTodos() {
         return fetch(`${this.uri}/listarTodos`).then(res => res.json()).catch(Error)
     }
-
 }
 
-class Categoria extends API {
-    constructor(categoriaUri="api/v1/categoria", basepath) {
-        super(basepath)
-        this.categoriaUri = categoriaUri
-        this.uri = `${this.basepath}${this.categoriaUri}`
-    }
-}
-
-export const API_categoria = new Categoria("v1/categoria/")
+export const DigitalBookingApi = new DigitalBookingAPI()
