ccdd0571e858913a9351c5ca4dc60c5a0baeadce
Fabricio Montivero <fabricio9027@gmail.com>
2022-11-07 10:55:15 -0500

    [MODIFICADO] se selecciono el efecto brillo para la carga, se cargan 6 cards con brillo. Se formateo el texto

--- client/src/components/Categorias/categorias.jsx
+++ client/src/components/Categorias/categorias.jsx
@@ -1,65 +1,99 @@
-import { useEffect, useState, useContext } from "react"
-import styles from "./categorias.module.css"
+import { useEffect, useState, useContext } from "react";
+import styles from "./categorias.module.css";
 import Contexto from "../../contexto/AppContext";
 import { DigitalBookingApi } from "../../data/conexionAPI";
 
 export default function Categorias() {
-    const [cargando, setEstaCargando] = useState(true)
-    const [categorias, setCategorias] = useState([])
-    const { filtarAutos }  = useContext(Contexto)
+  const [cargando, setEstaCargando] = useState(true);
+  const [categorias, setCategorias] = useState([]);
+  const { filtarAutos } = useContext(Contexto);
 
-    useEffect(()=>{
-        DigitalBookingApi
-        .categoria
-        .listarTodos()
-        .then(categorias => {
-            setCategorias(categorias)
-            setEstaCargando(false)
-        })
-    },[])
+  useEffect(() => {
+    DigitalBookingApi.categoria.listarTodos().then((categorias) => {
+      setCategorias(categorias);
+      setEstaCargando(false);
+    });
+  }, []);
 
-    if (cargando) {
-        return(
-            <section className={styles.contenedorPadre}>
-                <div className={styles.tituloCategorias}>
-                <div><h3>Elige tu categoria</h3></div> <div><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit</p></div>
-                </div>
+  if (cargando) {
+    return (
+      <section className={styles.contenedorPadre}>
+        <div className={styles.tituloCategorias}>
+          <div>
+            <h3>Elige tu categoria</h3>
+          </div>
+        </div>
 
-                <div className={styles.categoria_section}>
-                    {/* opcion 1 - spinner */}
-                    <article className={`${styles.categoria_article}`}>
-                        <div className={`spinner-border text-warning ${styles.categoria_loading_font}`} role="status"></div>
-                    </article>
-                    {/* opcion 2 - brillo */}
-                    <article className={`loading_background ${styles.categoria_loading_height}`}>
-                        <div className={`loading_animation`}></div>
-                    </article>
-                </div>
-            </section>
-        )
-    }
+        <div className={styles.categoria_section}>
+          <article className={`loading_background ${styles.categoria_loading_height}`}>
+            <div className={`loading_animation`}></div>
+          </article>
+          <article
+            className={`loading_background ${styles.categoria_loading_height}`}
+          >
+            <div className={`loading_animation`}></div>
+          </article>
+          <article
+            className={`loading_background ${styles.categoria_loading_height}`}
+          >
+            <div className={`loading_animation`}></div>
+          </article>
+          <article
+            className={`loading_background ${styles.categoria_loading_height}`}
+          >
+            <div className={`loading_animation`}></div>
+          </article>
+          <article
+            className={`loading_background ${styles.categoria_loading_height}`}
+          >
+            <div className={`loading_animation`}></div>
+          </article>
+          <article
+            className={`loading_background ${styles.categoria_loading_height}`}
+          >
+            <div className={`loading_animation`}></div>
+          </article>
+        </div>
+      </section>
+    );
+  }
 
-    return(
-        <section className={styles.contenedorPadre}>
-            <div className={styles.tituloCategorias}>
-                <div><h3>Elige tu categoria</h3></div>
-                <div><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit</p></div>
-            </div>
+  return (
+    <section className={styles.contenedorPadre}>
+      <div className={styles.tituloCategorias}>
+        <div>
+          <h3>Elige tu categoria</h3>
+        </div>
+        <div>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+        </div>
+      </div>
 
-            <div className={styles.categoria_section}>
-            {
-                categorias.map( cat => {
-                 return(
-                    <article className={styles.categoria_article} key={cat.id} onClick={(e) => { filtarAutos(cat.titulo)}}>
-                        <figure className={styles.categoria_figure}>
-                            <img src={cat.urlImagen} alt={cat.titulo} className={styles.categoria_img}/>
-                            <figcaption className={styles.categoria_figcaption}> {cat.titulo} </figcaption>
-                        </figure>
-                    </article>
-                    )
-                })
-            }
-            </div>
-        </section>
-    )
+      <div className={styles.categoria_section}>
+        {categorias.map((cat) => {
+          return (
+            <article
+              className={styles.categoria_article}
+              key={cat.id}
+              onClick={(e) => {
+                filtarAutos(cat.titulo);
+              }}
+            >
+              <figure className={styles.categoria_figure}>
+                <img
+                  src={cat.urlImagen}
+                  alt={cat.titulo}
+                  className={styles.categoria_img}
+                />
+                <figcaption className={styles.categoria_figcaption}>
+                  {" "}
+                  {cat.titulo}{" "}
+                </figcaption>
+              </figure>
+            </article>
+          );
+        })}
+      </div>
+    </section>
+  );
 }
