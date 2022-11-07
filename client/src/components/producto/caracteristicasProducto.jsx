b312fa8d1f65fed91a468e7bf9a80dd843dc4e86
NoeDelCorti <delcortinoelia@gmail.com>
2022-11-07 20:13:24 -0300

    Subiendo maqueta componentes descripcion producto, caracteristicas y politicas

--- client/src/components/producto/caracteristicasProducto.jsx
+++ client/src/components/producto/caracteristicasProducto.jsx
@@ -1,12 +1,40 @@
 import React from "react";
-import stylesCaracteristicasProd from './caracteristicasProducto.module.css';
+import style from './caracteristicasProducto.module.css';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faPerson,
+  faGauge,
+  faGasPump,
+  faCar,
+} from "@fortawesome/free-solid-svg-icons";
+
 
 export default function CaracteristicasProducto() {
     return (
       <section
-        className={stylesCaracteristicasProd.seccionCaracteristicasPorducto}
+        className={style.seccionCaracteristicasPorducto}
       >
-        CARACTERISTICAS PRODUCTO
+       <h3 className={style.tituloDescripcionProd}>
+       Encontrá tu auto
+      </h3>
+
+      <ul className={style.caracteristicas}>
+            <li>
+              <FontAwesomeIcon icon={faPerson} className={style.icons} /> 4
+              personas
+            </li>
+            <li>
+              <FontAwesomeIcon icon={faGauge} className={style.icons} /> 50
+              Litros
+            </li>
+            <li>
+              <FontAwesomeIcon icon={faGasPump} className={style.icons} /> Nafta
+            </li>
+            <li>
+              <FontAwesomeIcon icon={faCar} className={style.icons} />{" "}
+              Automático
+            </li>
+          </ul>
       </section>
     )
   }
