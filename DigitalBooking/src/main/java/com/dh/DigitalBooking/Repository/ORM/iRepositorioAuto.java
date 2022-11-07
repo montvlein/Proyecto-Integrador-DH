89e381336ced4f6265aa795aa57a83163724714d
Fabricio Montivero <fabricio9027@gmail.com>
2022-11-07 16:48:10 -0500

    [MODIFICADO] - se agregaron metodos para filtrar autos por categoria y/o ciudad

--- DigitalBooking/src/main/java/com/dh/DigitalBooking/Repository/ORM/iRepositorioAuto.java
+++ DigitalBooking/src/main/java/com/dh/DigitalBooking/Repository/ORM/iRepositorioAuto.java
@@ -1,18 +1,21 @@
 package com.dh.DigitalBooking.Repository.ORM;
 
 import com.dh.DigitalBooking.Models.Entities.Auto;
-import com.dh.DigitalBooking.Models.Entities.Categoria;
-import com.dh.DigitalBooking.Models.Entities.Ciudad;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface iRepositorioAuto extends JpaRepository<Auto, Long> {
 
-    //@Query("SELECT * FROM AUTO WHERE CIUDAD_ID = 1")
-    //List<Auto> buscarAutoPorCiudad(Ciudad ciudad);
+    @Query("select a from Auto a where a.categoria.Titulo = ?1")
+    List<Auto> buscarAutoPorCategoria(String parametro);
 
-    //@Query("SELECT * FROM AUTO WHERE CATEGORIA_ID = ?")
-    //List<Auto> buscarAutoPorCategoria(Categoria categoria);
+    @Query("select a from Auto a where a.ciudad.provincia = ?1")
+    List<Auto> buscarAutoPorCiudad(String parametro);
+
+    @Query("select a from Auto a where a.categoria.Titulo = ?1 and a.ciudad.provincia = ?2")
+    List<Auto> buscarAutoPor(String categoria, String ciudad);
 }
