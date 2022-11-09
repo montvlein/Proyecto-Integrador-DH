be53285e259e3aff671f355d26e86913599d69bc
NoeDelCorti <delcortinoelia@gmail.com>
2022-11-09 20:10:00 -0300

    [NUEVO] subiendo Cards principales destinos

--- client/src/components/cuerpo/destinos/principalesDestinos.jsx
+++ client/src/components/cuerpo/destinos/principalesDestinos.jsx
@@ -1,16 +1,33 @@
-import styles from "./destinos.module.css";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faStar } from "@fortawesome/free-solid-svg-icons";
+import styles from './destinos.module.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faStar } from '@fortawesome/free-solid-svg-icons'
 
 export default function Destinos() {
-  return (
-    <section className={styles.heroeContenedor}>
-      <h2>Principales destinos de Argentina</h2>
 
-      <div className={styles.contenidoPadre}>
-        
-        
+  return (
+    <section className={styles.destinoContenedor}>
+      <div  className={styles.heroeContenedor}>
+        <div  className={styles.tituloHeroe}>
+        <h2>Principales destinos de Argentina</h2>
+        </div>
+        <div className={styles.contenidoPadre}>
+          <div className={styles.cardDestino1}>
+          <p className={styles.atraccionesDestino}>
+            Camino de los 7 lagos
+          </p>
+          </div>
+          <div className={styles.cardDestino2}>
+          <p className={styles.atraccionesDestino}>
+            La Cumbrecita
+          </p>
+          </div>
+          <div className={styles.cardDestino3}>
+          <p className={styles.atraccionesDestino}>
+            Caviahue
+          </p>
+          </div>
+        </div>
       </div>
     </section>
-  );
+  )
 }
