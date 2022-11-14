c2935d6d5dcdbc41ee31589429a2d9849b44e648
NoeDelCorti <delcortinoelia@gmail.com>
2022-11-14 20:42:49 -0300

    [MODIFICACION] maquetado template reserva

--- client/src/components/Reserva/reserva.jsx
+++ client/src/components/Reserva/reserva.jsx
@@ -1,42 +1,24 @@
-import React from "react";
-import stylesArticlulo from './reservaContenedor.module.css';
-import Calendario from './calendario/calendario.module.css';
-import DetalleReserva from "./detalleReserva/detalleReserva.module.css";
-import FormDatos from './formDatos/formDatos.module.css';
-import HorarioReserva from './horarioReserva/horarioReserva.module.css';
+import React from 'react'
+import Calendario from './calendario/calendario'
+import DetalleReserva from './detalleReserva/detalleReserva'
+import FormDatos from './formDatos/formDatos'
+import HorarioReserva from './horarioReserva/horarioReserva'
+import CabeceraProducto from '../producto/cabeceraProducto'
+import PoliticasProducto from '../producto/politicasProducto'
 
 export default function Reserva() {
-    const { idProducto } = useParams()
-    const [cargando, setEstaCargando] = useState(true);
-    const [producto, setProducto] = useState({});
-    const [isOpen, setOpen] = useState(false);
-    const toggle = () => {
-      setOpen(!isOpen);
-    };
-  
-    useEffect(() => {
-      DigitalBookingApi.auto.buscarPorID(idProducto).then((auto) => {
-        setProducto(auto);
-        setEstaCargando(false);
-      });
-    }, []);
-  
-    if (cargando) {
-      return(
-        <article className={`${stylesArticlulo.reservaContenedor} d-flex justify-content-center`}>
-          <div className="spinner-border m-5" roler="status"></div>
-        </article>
-      )
-    }
-  
-    return (
-      <article className={`${stylesArticlulo.reservaContenedor} contenedor`}>
-        <Calendario />
-        <DetalleReserva />
-        <FormDatos/>
-        <HorarioReserva/>
-        
-      </article>
-    )
-  }
-  
+  return (
+      <section>
+        <CabeceraProducto />
+        <div>
+          <div>
+            <FormDatos />
+            <Calendario />
+            <HorarioReserva />
+          </div>
+            <DetalleReserva />
+        </div>
+        <PoliticasProducto />
+      </section>
+  )
+}
