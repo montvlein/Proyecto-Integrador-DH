11a9f1f116a912c30401e5126cf22699acbf057c
Maigualida <maigualidadelgado7@gmail.com>
2022-11-15 18:26:22 -0300

    [MODIFICO] Se modifico bloque reserva segun requerimientos [AGREGO] se agrego calendario y form a template de reserva

--- client/src/components/Reserva/calendario/calendario.jsx
+++ client/src/components/Reserva/calendario/calendario.jsx
@@ -1,9 +1,43 @@
 import React from "react";
+import styles from "../calendario/calendario.module.css"
+import "react-date-range/dist/styles.css"; // main css file
+import "react-date-range/dist/theme/default.css"; // theme css file
+import { useState } from "react";
+import { DateRange } from "react-date-range";
+import { addDays } from 'date-fns';
 
-export default function Calendario(){
+export default function HorarioReserva(){
+
+    const [state, setState] = useState([
+        {
+          startDate: new Date(),
+          endDate: new Date(),
+          key: "selection",
+        }
+      ]);
+
+
     return(
-        <section>
-            
-        </section>
+      <section className={styles.contenedorPadre}>
+      <div className={styles.tituloFechaReserva}>
+          <h3>Selecciona tu fecha de reserva</h3>
+      </div>
+           <div className={styles.fechaContenedor}>
+        <DateRange
+          editableDateInputs={true}
+          months={2}
+          onChange={(item) => setState([item.selection])}
+          moveRangeOnFirstSelection={false}
+          minDate={addDays(new Date(), -0)}
+          ranges={state}
+          direction="horizontal"
+          rangeColors={["#ffc107"]}
+          showMonthAndYearPickers={true}
+          showDateDisplay={false}
+          fixedHeight={true}
+        />
+       </div>
+      </section>
     )
 }
+
--- client/src/components/producto/bloqueReserva/bloqueReserva.jsx
+++ client/src/components/producto/bloqueReserva/bloqueReserva.jsx
@@ -4,33 +4,50 @@ import "react-date-range/dist/theme/default.css"; // theme css file
 import { useState } from "react";
 import { DateRange, DefinedRange } from "react-date-range";
 import { Link } from "react-router-dom";
+import { addDays } from "date-fns";
 
 function BloqueReserva() {
-  const [state, setState] = useState([
-    {
-      startDate: new Date(),
-      endDate: new Date(),
-      key: "selection",
-    },
-  ]);
+  const [state, setState] = useState([]);
 
   return (
     <section className={styles.contenedor}>
       <h3 className={styles.titulo}>Elegi la fecha de tu viaje</h3>
       <hr />
       <div className={styles.contenedorPadre}>
+        
         <div className={styles.fechaContenedor}>
           <DateRange
-            editableDateInputs={true}
+            editableDateInputs={false}
             months={2}
-            onChange={(item) => setState([item.selection])}
-            moveRangeOnFirstSelection={false}
-            ranges={state}
+            rangeColors={["#ffc107"]}
+            minDate={addDays(new Date(), -0)}
             direction="horizontal"
-            className={styles.calendario}
+            showSelectionPreview={true}
+            showPreview={false}
+            fixedHeight={true}
           />
-          <Link to="reserva" className={styles.botonReserva}>Iniciar Reserva</Link>
         </div>
+
+        <div className={styles.fechaContenedorResponsive}>
+          <DateRange
+            editableDateInputs={false}
+            months={1}
+            rangeColors={["#ffc107"]}
+            minDate={addDays(new Date(), -0)}
+            direction="horizontal"
+            showSelectionPreview={true}
+            showPreview={false}
+            fixedHeight={true}
+          />
+        </div>
+
+        <div className={styles.contenedorBoton}>
+          <h5>¡Inicia tu reserva ahora!</h5>
+          <Link to="reserva" className={styles.botonReserva}>
+            <button>Iniciar Reserva</button>
+          </Link>
+        </div>
+
       </div>
     </section>
   );
