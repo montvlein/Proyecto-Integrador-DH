164bd56eb9cd074ed565c21512948f2d670fdc1b
Fabricio Montivero <fabricio9027@gmail.com>
2022-11-16 17:21:59 -0500

    [NUEVO] - cambios menores

--- DigitalBooking/src/main/java/com/dh/DigitalBooking/Controller/ControladorUsuario.java
+++ DigitalBooking/src/main/java/com/dh/DigitalBooking/Controller/ControladorUsuario.java
@@ -15,7 +15,6 @@ import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.DisabledException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.*;
 
