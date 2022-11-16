27306315455618de9975d1290d75d283f004b183
unknown <abdalamilu@gmail.com>
2022-11-15 22:16:06 -0300

    exito files created

--- client/src/components/Exito/exito.jsx
+++ client/src/components/Exito/exito.jsx
@@ -1,7 +1,15 @@
 export default function Exito() {
     return(
         <section>
-            {/* Miyu aca va el html */}
+            {
+<div>
+    <img src="grupo-02\client\src\assets\check.png"></img>
+    <h1> ¡Muchas Gracias! </h1>
+    <p>Tu reserva ha sido exitosa</p>
+</div>
+
+
+            }
         </section>
     )
 }
