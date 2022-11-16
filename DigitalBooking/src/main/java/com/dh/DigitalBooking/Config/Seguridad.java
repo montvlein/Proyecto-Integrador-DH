09bbc214fe6fdbc21eae08fe4152edf2055fe9fb
Fabricio Montivero <fabricio9027@gmail.com>
2022-11-15 22:33:48 -0500

    [NUEVO] - se crearon clases configuracion para seguridad

---
+++ DigitalBooking/src/main/java/com/dh/DigitalBooking/Config/Seguridad.java
@@ -0,0 +1,23 @@
+package com.dh.DigitalBooking.Config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class Seguridad {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.cors().and().csrf().disable()
+                .authorizeRequests()
+                .antMatchers("**").permitAll()
+                .anyRequest()
+                .authenticated();
+
+        return http.build();
+    }
+}
