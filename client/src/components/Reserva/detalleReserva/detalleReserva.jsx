562d1c9eb5162ba70323bc2f68f523db72316935
NoeDelCorti <delcortinoelia@gmail.com>
2022-11-16 19:20:26 -0300

    [MODIFICACION] subiendo maquetacion de detalle de reserva

--- client/src/components/Reserva/detalleReserva/detalleReserva.jsx
+++ client/src/components/Reserva/detalleReserva/detalleReserva.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import CabeceraProducto from "../../producto/cabeceraProducto";
+import ImgProducto from "../../producto/imgProducto";
 export default function DetalleReserva(){
     return(
         <section>
+        <CabeceraProducto/>
+       
+        <div>
+        <h3>
+            Detalle de reserva
+        </h3>
+        <ImgProducto/>
+        <Categoria/>
+        
+        </div>
             
         </section>
     )
