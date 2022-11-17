161b54e2662e2870b84ac15217056340277c0346
NoeDelCorti <delcortinoelia@gmail.com>
2022-11-16 22:05:29 -0300

    [NUEVO] maquetado bloque detalle reserva

--- client/src/components/Reserva/detalleReserva/detalleReserva.jsx
+++ client/src/components/Reserva/detalleReserva/detalleReserva.jsx
@@ -1,20 +1,25 @@
 import React from "react";
-import CabeceraProducto from "../../producto/cabeceraProducto";
-import ImgProducto from "../../producto/imgProducto";
+import styles from "../detalleReserva/detalleReserva.module.css"
+import producto from "../../producto/cabeceraProducto"
+
+
 export default function DetalleReserva(){
     return(
-        <section>
-        <CabeceraProducto/>
-       
-        <div>
+        <section className={styles.detalleReserva}>
         <h3>
             Detalle de reserva
         </h3>
-        <ImgProducto/>
-        <Categoria/>
-        
-        </div>
-            
-        </section>
+        <img clasName={styles.imagenReserva} src= "https://0521ptc3n1db-g2-cliente.s3.us-east-2.amazonaws.com/imagenes/Compacto_Onix/Compacto_Onix-01.jpg"/>
+        <p>Nombre producto</p>
+        <div>Ubicacion producto</div>
+        <div>Check in</div>
+        <div>Check out</div>
+        <button>Confirmar Reserva</button>
+
+
+    
+    </section>
     )
 }
+
+
