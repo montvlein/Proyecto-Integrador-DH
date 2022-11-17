907b6f0d2f50c36cb649ac9dcbe8dd8d3f950310
unknown <abdalamilu@gmail.com>
2022-11-16 23:11:12 -0300

    some changes

--- client/src/components/Exito/exito.jsx
+++ client/src/components/Exito/exito.jsx
@@ -1,9 +1,15 @@
+import React from 'react'
+import styles from './exito.module.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+
+
 export default function Exito() {
     return(
         <section>
             {
 <div>
-    <img src="grupo-02\client\src\assets\check.png"></img>
+    
     <h1> ¡Muchas Gracias! </h1>
     <p>Tu reserva ha sido exitosa</p>
 </div>
