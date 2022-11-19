f06c6bfb7137c45b962a72fbd11b969d64b6ffb7
Fabricio Montivero <fabricio9027@gmail.com>
2022-11-19 15:30:14 -0500

    [MODIFICADO] - se agrego metodo para buscar por fecha

--- DigitalBooking/src/main/java/com/dh/DigitalBooking/Services/ServicioBuscador.java
+++ DigitalBooking/src/main/java/com/dh/DigitalBooking/Services/ServicioBuscador.java
@@ -6,6 +6,8 @@ import com.dh.DigitalBooking.Repository.ORM.iRepositorioAuto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.*;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 @Service
@@ -17,11 +19,24 @@ public class ServicioBuscador {
     @Autowired
     private ServicioAuto servicioProducto;
 
-    public List<AutoDTO> buscarAutoPor(String tituloCategoria, String nombreProvincia, String fechas) {
+    public List<Auto> buscarAutoPorFecha(String fechaInicio, String fechaFinal) {
+        LocalDate fInicio = LocalDate.parse(fechaInicio , DateTimeFormatter.ISO_LOCAL_DATE);
+        LocalDate fFinal = LocalDate.parse(fechaFinal, DateTimeFormatter.ISO_LOCAL_DATE);
+        return repositorioProducto.buscarAutoPorFecha(fInicio, fFinal);
+    }
+
+    public List<Auto> buscarAutoPorCategoria_Ciudad(String tituloCategoria, String nombreProvincia) {
         List<Auto> encontrados = null;
         if (nombreProvincia != null && tituloCategoria == null ) encontrados = repositorioProducto.buscarAutoPorCiudad(nombreProvincia);
         if (tituloCategoria != null && nombreProvincia == null ) encontrados = repositorioProducto.buscarAutoPorCategoria(tituloCategoria);
         if (nombreProvincia != null && tituloCategoria != null ) encontrados = repositorioProducto.buscarAutoPor(tituloCategoria, nombreProvincia);
+        return  encontrados;
+    }
+
+    public List<AutoDTO> buscarAutoPor(String tituloCategoria, String nombreProvincia, String fechaInicio, String fechaFinal) {
+        List<Auto> encontrados = null;
+        if (tituloCategoria != null || nombreProvincia != null ) encontrados = buscarAutoPorCategoria_Ciudad(tituloCategoria, nombreProvincia);
+        if (fechaInicio != null || fechaFinal != null ) encontrados = buscarAutoPorFecha(fechaInicio, fechaFinal);
         return  servicioProducto.autoToDTO(encontrados);
     }
 
