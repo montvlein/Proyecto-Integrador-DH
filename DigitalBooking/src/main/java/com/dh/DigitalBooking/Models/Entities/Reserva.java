5979cafebc6e4e1015f1b76e8e52ebff3ec0cbd6
Fabricio Montivero <fabricio9027@gmail.com>
2022-11-19 15:28:31 -0500

    [MODIFICADO] - cambiaron los tidos de datos de LocalDateTime a LocalDate o LocalTime

--- DigitalBooking/src/main/java/com/dh/DigitalBooking/Models/Entities/Reserva.java
+++ DigitalBooking/src/main/java/com/dh/DigitalBooking/Models/Entities/Reserva.java
@@ -5,7 +5,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
-import java.time.LocalDateTime;
+import java.time.*;
 
 @Getter
 @Setter
@@ -15,9 +15,9 @@ public class Reserva {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private LocalDateTime horaComienzoReserva;
-    private LocalDateTime fechaInicialReserva;
-    private LocalDateTime fechaFinalReserva;
+    private LocalTime horaComienzoReserva;
+    private LocalDate fechaInicialReserva;
+    private LocalDate fechaFinalReserva;
 
     @ManyToOne
     @JoinColumn(name= "auto_id")
