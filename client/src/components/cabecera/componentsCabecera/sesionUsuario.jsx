9a5216ccfc3c1b4c76a804f11d887d462743f3f1
Fabricio Montivero <fabricio9027@gmail.com>
2022-12-02 22:36:36 -0500

    se agrego texto para saber si el usuario es admin o no

--- client/src/components/cabecera/componentsCabecera/sesionUsuario.jsx
+++ client/src/components/cabecera/componentsCabecera/sesionUsuario.jsx
@@ -5,39 +5,44 @@ import Contexto from "../../../contexto/AppContext";
 import { useContext } from "react";
 
 function UsuarioIniciado() {
-  const { getUsuario } = useContext(Contexto)
-  const { cerrarSesion } = useContext(Contexto)
+  const { getUsuario } = useContext(Contexto);
+  const { cerrarSesion } = useContext(Contexto);
 
   return (
     <section>
-    <div className={styles.padreSesion}>
+      <div className={styles.padreSesion}>
+        { getUsuario().esAdmin?
+          <p className={styles.administrador}>Administrador</p>:
+          null
+        }
 
         <div className={styles.iconoSesion}>
           <h6>
-            {getUsuario().nombre.charAt(0).toUpperCase()}{getUsuario().apellido.charAt(0).toUpperCase()}
-            </h6>
+            {getUsuario().nombre.charAt(0).toUpperCase()}
+            {getUsuario().apellido.charAt(0).toUpperCase()}
+          </h6>
         </div>
 
-      <div className={styles.contenedorNombreSesion}>
-        <dl>
-          <dt className={styles.saludo}>Hola,</dt>
-          <dl className={styles.nombreUsuario}>
-          {getUsuario().nombre.charAt(0).toUpperCase() + getUsuario().nombre.slice(1)}{" "} 
-          {getUsuario().apellido.charAt(0).toUpperCase() + getUsuario().apellido.slice(1)}</dl>
-        </dl>
+        <div className={styles.contenedorNombreSesion}>
+          <dl>
+            <dt className={styles.saludo}>Hola,</dt>
+            <dl className={styles.nombreUsuario}>
+              {getUsuario().nombre.charAt(0).toUpperCase() +
+                getUsuario().nombre.slice(1)}{" "}
+              {getUsuario().apellido.charAt(0).toUpperCase() +
+                getUsuario().apellido.slice(1)}
+            </dl>
+          </dl>
+        </div>
+
+        <div className={styles.iconoCerrar}>
+          <FontAwesomeIcon
+            icon={faXmark}
+            onClick={cerrarSesion}
+          ></FontAwesomeIcon>
+        </div>
       </div>
-
-      <div className={styles.iconoCerrar}>
-        <FontAwesomeIcon icon={faXmark} onClick={cerrarSesion}></FontAwesomeIcon>
-      </div>
-    </div>
-
-</section>
-
-
-
-
-
+    </section>
   );
 }
 
